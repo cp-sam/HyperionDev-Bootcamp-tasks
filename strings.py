39be829606aab4bf19f744243e8e0eb1936eed1f
Samuel Chukwujekwu <sparkzinwizle@gmail.com>
2023-06-22 19:39:43 +0100

    Added extra functionality to allow users enter their names and receive a greeting

--- strings.py
+++ strings.py
@@ -1,3 +1,7 @@
 # The code below removes the '$' character from the string
 hero = "$$$Superman$$$"
 print(hero.strip("$"))
+
+user = input("Enter your name: ")
+result = f"Hello {user}"
+print(result)
